19e3d7dbfe9b4817966d2c7067f384a1cd68df8a
James SETKEH Griffis <setkeh@gmail.com>
2021-02-10 09:17:26 +1100

    Adding a little more debug

--- main/ota.h
+++ main/ota.h
@@ -4,7 +4,7 @@ extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
 
 #define PROJECT_NAME      CONFIG_PROJECT_NAME
-#define FIRMWARE_VERSION	1.5
+#define FIRMWARE_VERSION	1.6
 #define UPDATE_JSON_URL		CONFIG_FIRMWARE_UPGRADE_JSON_URL
 
 // receive buffer
